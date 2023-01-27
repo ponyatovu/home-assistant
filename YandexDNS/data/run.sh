9b883428dcd03dd3a17541cd2f90b10adfcd1d2d
ponyatovu <user_for_all@mail.ru>
2023-01-27 10:14:41 +0600

    bug fix

--- YandexDNS/data/run.sh
+++ YandexDNS/data/run.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/with-contenv bashio
 
-source trycatch.sh
+function try()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function throw()
+{
+    exit $1
+}
+
+function catch()
+{
+    export exception_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $exception_code
+}
 
 # Define custom exception types
 export ERR_BAD=100
